1f63e2dcc08faf77cfb011057e4531483df3fefe
coldreefer <emrcopsai@gmail.com>
2025-12-18 11:37:25 -0300

    Update and rename orgchart.js to script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const chart = new d3.OrgChart()
+  .container('#chart')
+  .nodeWidth(() => 220)
+  .nodeHeight(() => 130)
+  .childrenMargin(() => 40)
+  .compactMarginBetween(() => 30)
+  .compactMarginPair(() => 50)
+  .nodeContent(d => {
+    const img = d.data.ImageURL || '';
+    return `
+      <div class="org-node">
+        <img src="${img}">
+        <div class="name">
+          ${d.data["First name (required)"]} ${d.data["Last name (required)"]}
+        </div>
+        <div class="role">${d.data.Position || ''}</div>
+      </div>
+    `;
+  })
+  .onNodeClick(d => {
+    chart.toggleNode(d.data["Email (required)"]).render();
+  });
+
+Papa.parse("team.csv", {
+  download: true,
+  header: true,
+  skipEmptyLines: true,
+  complete: function (res) {
+
+    const data = res.data
+      .filter(d => d["Email (required)"])
+      .map(d => ({
+        ...d,
+        id: d["Email (required)"],
+        parentId: d["SupervisorEmail (required)"] || null
+      }));
+
+    chart
+      .data(data)
+      .render();
+  }
+});
